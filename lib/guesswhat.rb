b62893b245e99d0b592bf191d2ad3e04546d1a9b
Sam Assadi <samsassadi@gmail.com>
2016-05-05 14:18:31 -0700

    add image functionality

--- lib/guesswhat.rb
+++ lib/guesswhat.rb
@@ -1,5 +1,24 @@
-require "guesswhat/version"
+# require "guesswhat/version"
+require 'rest-client'
 
 module Guesswhat
-  # Your code goes here...
+  class Product
+
+    def self.image(item)
+
+      #Default URL for finding images from google
+      url = "https://www.googleapis.com/customsearch/v1?q=#{item}&cx=008288361057280940904%3Acaao_sbv0xy&fileType=png&filter=1&imgSize=xlarge&num=1&searchType=image&key=AIzaSyBvSuUjfRZ0ImQP6KEgDdQEtdUoB1nWQY8"
+      response = JSON.parse(RestClient.get url)
+
+      #Finding the first image from Google Images
+      if response["items"][0]["link"] != nil
+        image_url = response["items"][0]["link"]
+      else
+        image_url = nil
+      end
+
+      puts image_url
+    end
+  end
 end
+
